0cf10150d1eb14b3d4b95d75d8b8aa9971bfac0a
Varun Pradhan <69717481+varun-pradhan@users.noreply.github.com>
2024-01-30 13:24:59 +0530

    Adding Components and assets for the app.

---
+++ ConsoleButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="button">
+    <button type="button" @click="click"
+   
+    >New Task</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ConsoleButton",
+  methods: {
+    click() {
+      // alert("Hello There !!");
+      this.$emit("addtask-Toggle"); // create an emit event to be triggered when
+       //above click event is called simultaneously [ call the emit where component is used created ]
+    },
+  },
+};
+</script>
+
+<style>
+.button {
+  padding: 0.5em;
+  border: 0.2em dashed green;
+  border-radius: 50%;
+}
+</style>
---
+++ TaskComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <!-- <div class="task"> -->
+    <h3
+     :class=" [ item.remainder ? 'remainder' : ''  ] "
+      @dblclick="AllTasks.toggleRemainder(item.id)"  >
+        {{ item.task }}
+        <i @click="AllTasks.deleteTask(item.id)" class="fas fa-xmark"></i>
+    </h3>
+
+    <!-- </div> -->
+</template>
+    
+<script setup>
+import { useTaskStore } from "../store/index.js";
+const AllTasks = useTaskStore();
+import { defineProps } from "vue";
+
+defineProps({
+    item: Object,
+});
+</script>
+    
+<style scoped>
+.remainder {
+    border: 3px dashed purple;
+}
+div{
+
+    overflow: hidden;
+}
+
+.fas {
+    color: rgb(164, 47, 47);
+    padding : 0 30px;
+    float :right;
+    /* border: 0.1em red dashed */
+}
+
+/*
+        table, th,td {
+            border: 2px rgb(145, 75, 145) double ;
+            border-collapse: collapse;
+            padding: 0.3em;
+            margin : 1em auto;
+    
+        }
+    
+        .table{
+            overflow-x: auto;
+        }
+    
+        {
+            border:2px solid;
+            padding:0.3em
+        }*/</style>
