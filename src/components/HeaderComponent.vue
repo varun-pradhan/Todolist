236ea2f9d3254bc9c226274a5e271f1999dda576
Varun Pradhan <69717481+varun-pradhan@users.noreply.github.com>
2024-01-30 13:36:51 +0530

    Add files via upload

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="header">
+        <h1> To Do List App<br /> {{ data }} </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    props: {
+        'data': {
+            type: String,
+            default: 'Component'
+        }
+    }
+} 
+</script>
+
+<style scoped>
+.header {
+    background-color: rgb(249, 216, 173);
+    border: 0.1em red dashed;
+
+}
+</style>
---
+++ src/components/TaskListComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <!-- <h3>
+    {{ item.task }}
+      <i @click=" AllTasks.deleteTask(item.id)" class="fas  fa-xmark"></i>
+    </h3> -->
+  <div v-for="item in atask" v-bind:key="item">
+    <TaskComponent :item=item />
+  </div>
+  <!-- <h3>{{ AllTasks.corns}}</h3> -->
+
+  <!-- <h3>{{ AllTasks.getTask() }} -->
+  <!-- {{task.task}} -->
+  <!-- </h3> -->
+</template>
+    
+    <script setup>
+import { useTaskStore } from "../store/index.js";
+import { storeToRefs } from "pinia";
+const AllTasks = useTaskStore();
+
+import TaskComponent from "./TaskComponent.vue";
+
+const { atask } = storeToRefs(AllTasks);
+// export default{
+//     name : 'TaskListComponent',
+
+//     props:{
+//         AllTasks : Array,
+//     }
+
+// }
+</script>
+    
+    <style scoped>
+/* div{
+        border: 3px dashed black
+    } */
+
+/*
+        table, th,td {
+            border: 2px rgb(145, 75, 145) double ;
+            border-collapse: collapse;
+            padding: 0.3em;
+            margin : 1em auto;
+    
+        }
+    
+        .table{
+            overflow-x: auto;
+        }
+        {
+            border:2px solid;
+            padding:0.3em
+        }*/
+</style>
